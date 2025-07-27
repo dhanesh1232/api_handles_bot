876bac4a75316b09ba45b21ba48f92f3fadbeb40
dhanesh1232 <dhaneshreddy980@gmail.com>
2025-07-27 23:28:58 +0530

    Initial server commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// backend-server/index.js
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+const server = http.createServer(app);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000", // Or your frontend URL
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send-message", (msg) => {
+    console.log("Received:", msg);
+    io.emit("receive-message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Socket server running at http://localhost:${PORT}`);
+});
